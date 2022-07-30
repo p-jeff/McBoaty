65b9ab1059f3f4800c764fd4d76727d97e02b0d6
Johannes <84457538+p-jeff@users.noreply.github.com>
2022-07-30 13:12:39 +0200

    Quick edits

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
-import static_adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static';
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: static_adapter({
+		adapter: adapter({
 			paths: {
-				base: '/McBoaty',
-				assets: '/McBoaty'
+				base: dev ? '' : '/McBoaty',
 			}
 		}),
 		prerender: { default: true }
