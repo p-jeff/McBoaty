550ab292afaf9880cdc092228ab98ea0c9adc5e5
Johannes <84457538+p-jeff@users.noreply.github.com>
2022-07-30 14:22:01 +0200

    Fix from issue #5668

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
-const dev = process.env.NODE_ENV === 'development';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			paths: {
-				base: dev ? '' : '/McBoaty',
-			}
-		}),
-		prerender: { default: true }
+		adapter: adapter(),
+		prerender: { default: true },
+		paths: { base: "/McBoaty" }
+	},
 
-	}
 };
 
 export default config;
